35defd04bc0a884a83ff037b11fe1d34a3f06ab9
derfei <longyiyuan@outlook.com>
2020-12-12 21:04:56 +0800

    完成爬虫部分的爬取代码，目前存在不能爬取第二级页面的问题，还需要进行调试

--- Spiders/ArvixSpider/ArvixSpider/spiders/TodayPapers.py
+++ Spiders/ArvixSpider/ArvixSpider/spiders/TodayPapers.py
@@ -34,7 +34,7 @@ class TodaypapersSpider(scrapy.Spider):
                 if topSubject not in subjectTree:
                     subjectTree[topSubject] = []
                 subjectTree[topSubject].append(itemName)
-                subjectLinks[itemName] = response.urljoin(itemLinks)
+                subjectLinks[itemName] = response.urljoin(itemLinks[1])
 
                 # if the item include something
                 tmpTextList = tmpli.css("::text").getall()
@@ -52,7 +52,7 @@ class TodaypapersSpider(scrapy.Spider):
                         subjectLinks[subitemName] = response.urljoin(subitemLink)
 
                         # 爬取数据
-                        yield scrapy.Request(link=subitemLink, callback=self.parse_pages)
+                        yield scrapy.http.Request(response.urljoin(subitemLink), callback=self.parse_pages)
 
     def parse_pages(self, response):
         # 看一下一共有多少页 selector=#dlpage > small:nth-child(4) /html/body/div[5]/div/small[1]  
@@ -63,7 +63,7 @@ class TodaypapersSpider(scrapy.Spider):
         onepageUrl = r"pastweek?skip={0}&show={1}".format(0, entNum)
         onepageUrl = "{0}{1}".format(response.url.replace("recent", ""), onepageUrl)
 
-        yield scrapy.Request(link=onepageUrl, callback=self.parse_entries)
+        yield scrapy.http.Request(onepageUrl, callback=self.parse_entries)
 
 
     def parse_entries(self, response):
@@ -75,14 +75,28 @@ class TodaypapersSpider(scrapy.Spider):
             entUrl = tmpentry.css(r"span > a:nth-child(1)::attr(href)").get()
 
             # 根据链接，爬取论文摘要信息
-            yield scrapy.response(link=response.urljoin(entUrl), callback=parse_abs)
+            yield scrapy.Request(response.urljoin(entUrl), callback=parse_abs)
 
     def parse_abs(self, response):
 
         # 爬取论文的数据信息
         absUrl = response.url
         pdfUrl = response.css(r"#abs-outer > div.extra-services > div.full-text > ul > li:nth-child(1) > a ::attr(href)").get()
-        title = response.css(r"#abs > h1 ::text").get()
+        title = response.css(r"#abs > h1 ::text").getall()[1]
+        authors = response.css(r"#abs > div.authors > a ::text").getall()
+        absText = response.css(r"#abs > blockquote ::text").getall()[2].replace('\n', '').strip()
+        comments = response.css(r"#abs > div.metatable > table  > tr:nth-child(1) > td.tablecell.comments.mathjax ::text").get()
+        subjects = response.css(r"#abs > div.metatable > table > tbody > tr:nth-child(2) > td.tablecell.subjects ::text").getall()
+        submitDate = response.css(r"#abs-outer > div.leftcolumn > div.submission-history ::text").getall()[6]
+        updateDate = response.css(r"#abs-outer > div.leftcolumn > div.submission-history ::text").getall()[-1]
 
-        pass
-        
+        # 简单清洗数据，去除所有的换行
+        title = title.replace('\n', '')
+        authors = [tmp.replace('\n', '') for tmp in authors]
+        comments = comments.replace('\n', '')
+        subjects = [tmp.replace('\n', '') for tmp in subjects]
+
+
+        # 返回数据
+        return {'absUrl': absUrl, 'pdfUrl': pdfUrl, 'title': title, 'authors': authors, 'absText': absText, 
+        'comments': comments, 'subjects': subjects, 'submitDate': submitDate, 'updateDate': updateDate}
